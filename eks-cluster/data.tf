b382371a71f367e2e446b29df485ed930b91e60e
daya9096 <darshantm121@gamil.com>
2026-09-01 17:33:50 +0530

    update

--- eks-cluster/data.tf
+++ eks-cluster/data.tf
@@ -14,8 +14,4 @@ data "aws_region" "current" {}
 # KMS Key
 ##########################################
 
-data "aws_kms_alias" "eks" {
 
-  name = "alias/${var.project_name}-${var.environment}-eks"
-
-}
